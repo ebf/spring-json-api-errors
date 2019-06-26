bab5fa1e552e44191550492b8434d88cb5a6e62a
Vladimir Spasic <vladimir.spasic.86@gmail.com>
2019-06-26 14:13:32 +0200

    update repo urls

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,7 +61,7 @@ publishing {
         maven {
             if(project.version.toString().contains("-SNAPSHOT")) {
                 name = "ebf-snapshots"
-                url = uri("http://repository.dev.ebf.de/nexus/content/repositories/releases/")
+                url = uri("http://repository.dev.ebf.de/nexus/content/repositories/snapshots/")
             } else {
                 name = "ebf-releases"
                 url = uri("http://repository.dev.ebf.de/nexus/content/repositories/releases/")
